a8940de216f14eb529805e0ebe3e478c46509602
jzamesdean <k5052402@naver.com>
2023-04-24 20:17:28 +0900

    반복과정 줄이기

---
+++ chap01/alternative1a.py
@@ -0,0 +1,13 @@
+# +와 -를 번갈아 출력하기 1(for문 수정)
+
+print('+와 -를 번갈아 출력합니다.')
+n = int(input('몇 개를 출력할까요? : '))
+
+for i in range(1,n+1) :
+    if i%2 :
+        print('+',end='') #홀수
+    else :
+        print('-',end='') #짝수
+        
+print()
+        
